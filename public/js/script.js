c318f3e8daa1ffdf079c8419d80ecb2fe0e44304
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-11-06 21:58:37 +0530

    bugs fixed

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
 
-const startButton = document.getElementById('start-btn')
+const startButton = document.getElementById('start-btn');
 const nextButton = document.getElementById('next-btn')
 const submitButton = document.getElementById('submit-btn')
 const questionContainerElement = document.getElementById('question-container')
@@ -17,7 +17,7 @@ let shuffledQuestions, currentQuestionIndex;
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
   currentQuestionIndex++;
-  setNextQuestion()
+  setNextQuestion();
 })
 
 
@@ -59,7 +59,7 @@ function startGame() {
   setNextQuestion()
 }
 
-
+// Shuffel Functionality Added
 function setNextQuestion() {
   resetState()
   showQuestion(shuffledQuestions[currentQuestionIndex])
@@ -76,6 +76,7 @@ function showQuestion(question) {
     }
     button.addEventListener('click', selectAnswer);
     answerButtonsElement.appendChild(button)
+
   })
 }
 
@@ -129,104 +130,7 @@ function clearStatusClass(element) {
   element.classList.remove('wrong')
 }
 
-
-if (subject == "Random") {
-  questions = [
-    {
-      question: 'Who is creator of this quiz?',
-      answers: [
-        { text: 'Aayush jain', correct: true },
-        { text: 'Aayush jain', correct: true },
-        { text: 'Aayush jain', correct: true },
-        { text: 'Aayush jain', correct: true }
-      ]
-    },
-    {
-      question: 'Who is the best YouTuber?',
-      answers: [
-        { text: 'Ashish Chanchlani', correct: false },
-        { text: 'Pewdiepie', correct: false },
-        { text: 'Logan Paul', correct: false },
-        { text: 'Ankur Warikoo', correct: true }
-      ]
-    },
-    {
-      question: 'Is web development fun?',
-      answers: [
-        { text: 'Kinda', correct: false },
-        { text: 'YES!!!', correct: true },
-        { text: 'Um no', correct: false },
-        { text: 'IDK', correct: false }
-      ]
-    },
-    {
-      question: 'What is 2 + 2?',
-      answers: [
-        { text: '4', correct: true },
-        { text: '22', correct: false }
-      ]
-    },
-    {
-      question: 'What is 4 * 2?',
-      answers: [
-        { text: '6', correct: false },
-        { text: '8', correct: true }
-      ]
-    }
-  ]
-}
-
-else if (subject == "Biology") {
-  questions = [
-    {
-      question: 'Which of the following protects the body against infectious disease and foreign invaders?',
-      answers: [
-        { text: 'Leukocytes', correct: true },
-        { text: 'Red blood cells', correct: false },
-        { text: 'Endoplasmic reticulum (ER)', correct: false },
-        { text: 'Golgi apparatus', correct: false }
-      ]
-    },
-    {
-      question: 'Hemoglobin in humans has the highest affinity for which of the following gases?',
-      answers: [
-        { text: 'Methane', correct: false },
-        { text: 'Carbon Monoxide', correct: true },
-        { text: 'Nitrous oxide', correct: false },
-        { text: 'Carbon dioxide', correct: false }
-      ]
-    },
-    {
-      question: 'Which of the following is not a part of the digestive system?',
-      answers: [
-        { text: 'Small Intestine', correct: false },
-        { text: 'Rectum', correct: false },
-        { text: 'Pharynx', correct: false },
-        { text: 'Spleen', correct: true }
-      ]
-    },
-    {
-      question: 'From which among the following parts of a plant Cinnamon is obtained?',
-      answers: [
-        { text: 'Leaves', correct: false },
-        { text: 'Seeds', correct: false },
-        { text: 'Bark', correct: true },
-        { text: 'Buds', correct: false }
-      ]
-    },
-    {
-      question: 'Which among the following plants is used as Green Manure?',
-      answers: [
-        { text: 'Wheat', correct: false },
-        { text: 'Sunhemp', correct: true },
-        { text: 'Cotton', correct: false },
-        { text: 'Rice', correct: false }
-      ]
-    }
-  ]
-}
-
-else if (subject == "Physics") {
+if (subject == "Physics") {
   questions = [
     {
       question: 'Who gave the Theory of Relativity',
@@ -276,102 +180,227 @@ else if (subject == "Physics") {
   ]
 }
 
-else if (subject == "Tech") {
-  questions = [
-    {
-      question: 'In which decade was the American Institute of Electrical Engineers (AIEE) founded?',
-      answers: [
-        { text: '1850s', correct: false },
-        { text: '1880s', correct: true },
-        { text: '1930s', correct: false },
-        { text: '1950s', correct: false }
-      ]
-    },
-    {
-      question: 'What is part of a database that holds only one type of information?',
-      answers: [
-        { text: 'Report', correct: false },
-        { text: 'Field', correct: true },
-        { text: 'Record', correct: false },
-        { text: 'File', correct: false }
-      ]
-    },
-    {
-      question: '"OS" computer abbreviation usually means ?',
-      answers: [
-        { text: 'Order of Significance', correct: false },
-        { text: 'Open Software', correct: false },
-        { text: 'Operating System', correct: true },
-        { text: 'Optical Sensor', correct: false }
-      ]
-    },
-    {
-      question: '".MOV" extension refers usually to what kind of file?',
-      answers: [
-        { text: 'Image file', correct: false },
-        { text: 'Animation/movie file', correct: true },
-        { text: 'Audio file', correct: false },
-        { text: 'MS Office document', correct: false }
-      ]
-    },
-    {
-      question: 'Which is a type of Electrically-Erasable Programmable Read-Only Memory?',
-      answers: [
-        { text: 'Flash', correct: true },
-        { text: 'Flange', correct: false },
-        { text: 'Fury', correct: false },
-        { text: 'FRAM', correct: false }
-      ]
-    }
-  ]
-}
 
-else if (subject == "History") {
-  questions = [
-    {
-      question: 'The rulers of which dynasty were first to make land grants to Brahmanas?',
-      answers: [
-        { text: 'Maurya', correct: false },
-        { text: 'Sunga', correct: false },
-        { text: 'Satvahana', correct: true },
-        { text: 'Kanvas', correct: false }
-      ]
-    },
-    {
-      question: 'First Buddhist Council was held during the reign of which of the following kings?',
-      answers: [
-        { text: 'Ajatsatru', correct: true },
-        { text: 'Ashoka', correct: false },
-        { text: 'Kanishka', correct: false },
-        { text: 'Kalasoka', correct: false }
-      ]
-    },
-    {
-      question: 'The University of Nalanda was set up by which Gupta ruler?',
-      answers: [
-        { text: 'Kumargupta I', correct: true },
-        { text: 'Chandragupta II', correct: false },
-        { text: 'Samudragupta', correct: false },
-        { text: 'Kumargupta II', correct: false }
-      ]
-    },
-    {
-      question: 'Who was the Syrian ambassador in the court of Bindusara?',
-      answers: [
-        { text: 'Megasthenes', correct: false },
-        { text: 'Dimachos', correct: true },
-        { text: 'Dionysius', correct: false },
-        { text: 'Amitrochates', correct: false }
-      ]
-    },
-    {
-      question: 'The Kalinga war was fought in which year of Ashoka reign?',
-      answers: [
-        { text: '6', correct: false },
-        { text: '7', correct: false },
-        { text: '8', correct: true },
-        { text: '9', correct: false }
-      ]
-    }
-  ]
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//else if (subject == "Random") {
+//   questions = [
+//     {
+//       question: 'Who is creator of this quiz?',
+//       answers: [
+//         { text: 'Aayush jain', correct: true },
+//         { text: 'Aayush jain', correct: true },
+//         { text: 'Aayush jain', correct: true },
+//         { text: 'Aayush jain', correct: true }
+//       ]
+//     },
+//     {
+//       question: 'Who is the best YouTuber?',
+//       answers: [
+//         { text: 'Ashish Chanchlani', correct: false },
+//         { text: 'Pewdiepie', correct: false },
+//         { text: 'Logan Paul', correct: false },
+//         { text: 'Ankur Warikoo', correct: true }
+//       ]
+//     },
+//     {
+//       question: 'Is web development fun?',
+//       answers: [
+//         { text: 'Kinda', correct: false },
+//         { text: 'YES!!!', correct: true },
+//         { text: 'Um no', correct: false },
+//         { text: 'IDK', correct: false }
+//       ]
+//     },
+//     {
+//       question: 'What is 2 + 2?',
+//       answers: [
+//         { text: '4', correct: true },
+//         { text: '22', correct: false }
+//       ]
+//     },
+//     {
+//       question: 'What is 4 * 2?',
+//       answers: [
+//         { text: '6', correct: false },
+//         { text: '8', correct: true }
+//       ]
+//     }
+//   ]
+// }
+
+// else if (subject == "Biology") {
+//   questions = [
+//     {
+//       question: 'Which of the following protects the body against infectious disease and foreign invaders?',
+//       answers: [
+//         { text: 'Leukocytes', correct: true },
+//         { text: 'Red blood cells', correct: false },
+//         { text: 'Endoplasmic reticulum (ER)', correct: false },
+//         { text: 'Golgi apparatus', correct: false }
+//       ]
+//     },
+//     {
+//       question: 'Hemoglobin in humans has the highest affinity for which of the following gases?',
+//       answers: [
+//         { text: 'Methane', correct: false },
+//         { text: 'Carbon Monoxide', correct: true },
+//         { text: 'Nitrous oxide', correct: false },
+//         { text: 'Carbon dioxide', correct: false }
+//       ]
+//     },
+//     {
+//       question: 'Which of the following is not a part of the digestive system?',
+//       answers: [
+//         { text: 'Small Intestine', correct: false },
+//         { text: 'Rectum', correct: false },
+//         { text: 'Pharynx', correct: false },
+//         { text: 'Spleen', correct: true }
+//       ]
+//     },
+//     {
+//       question: 'From which among the following parts of a plant Cinnamon is obtained?',
+//       answers: [
+//         { text: 'Leaves', correct: false },
+//         { text: 'Seeds', correct: false },
+//         { text: 'Bark', correct: true },
+//         { text: 'Buds', correct: false }
+//       ]
+//     },
+//     {
+//       question: 'Which among the following plants is used as Green Manure?',
+//       answers: [
+//         { text: 'Wheat', correct: false },
+//         { text: 'Sunhemp', correct: true },
+//         { text: 'Cotton', correct: false },
+//         { text: 'Rice', correct: false }
+//       ]
+//     }
+//   ]
+// }
+
+
+
+// else if (subject == "Tech") {
+//   questions = [
+//     {
+//       question: 'In which decade was the American Institute of Electrical Engineers (AIEE) founded?',
+//       answers: [
+//         { text: '1850s', correct: false },
+//         { text: '1880s', correct: true },
+//         { text: '1930s', correct: false },
+//         { text: '1950s', correct: false }
+//       ]
+//     },
+//     {
+//       question: 'What is part of a database that holds only one type of information?',
+//       answers: [
+//         { text: 'Report', correct: false },
+//         { text: 'Field', correct: true },
+//         { text: 'Record', correct: false },
+//         { text: 'File', correct: false }
+//       ]
+//     },
+//     {
+//       question: '"OS" computer abbreviation usually means ?',
+//       answers: [
+//         { text: 'Order of Significance', correct: false },
+//         { text: 'Open Software', correct: false },
+//         { text: 'Operating System', correct: true },
+//         { text: 'Optical Sensor', correct: false }
+//       ]
+//     },
+//     {
+//       question: '".MOV" extension refers usually to what kind of file?',
+//       answers: [
+//         { text: 'Image file', correct: false },
+//         { text: 'Animation/movie file', correct: true },
+//         { text: 'Audio file', correct: false },
+//         { text: 'MS Office document', correct: false }
+//       ]
+//     },
+//     {
+//       question: 'Which is a type of Electrically-Erasable Programmable Read-Only Memory?',
+//       answers: [
+//         { text: 'Flash', correct: true },
+//         { text: 'Flange', correct: false },
+//         { text: 'Fury', correct: false },
+//         { text: 'FRAM', correct: false }
+//       ]
+//     }
+//   ]
+// }
+
+// else if (subject == "History") {
+//   questions = [
+//     {
+//       question: 'The rulers of which dynasty were first to make land grants to Brahmanas?',
+//       answers: [
+//         { text: 'Maurya', correct: false },
+//         { text: 'Sunga', correct: false },
+//         { text: 'Satvahana', correct: true },
+//         { text: 'Kanvas', correct: false }
+//       ]
+//     },
+//     {
+//       question: 'First Buddhist Council was held during the reign of which of the following kings?',
+//       answers: [
+//         { text: 'Ajatsatru', correct: true },
+//         { text: 'Ashoka', correct: false },
+//         { text: 'Kanishka', correct: false },
+//         { text: 'Kalasoka', correct: false }
+//       ]
+//     },
+//     {
+//       question: 'The University of Nalanda was set up by which Gupta ruler?',
+//       answers: [
+//         { text: 'Kumargupta I', correct: true },
+//         { text: 'Chandragupta II', correct: false },
+//         { text: 'Samudragupta', correct: false },
+//         { text: 'Kumargupta II', correct: false }
+//       ]
+//     },
+//     {
+//       question: 'Who was the Syrian ambassador in the court of Bindusara?',
+//       answers: [
+//         { text: 'Megasthenes', correct: false },
+//         { text: 'Dimachos', correct: true },
+//         { text: 'Dionysius', correct: false },
+//         { text: 'Amitrochates', correct: false }
+//       ]
+//     },
+//     {
+//       question: 'The Kalinga war was fought in which year of Ashoka reign?',
+//       answers: [
+//         { text: '6', correct: false },
+//         { text: '7', correct: false },
+//         { text: '8', correct: true },
+//         { text: '9', correct: false }
+//       ]
+//     }
+//   ]
+// }
